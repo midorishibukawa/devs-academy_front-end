d8cebccbeeef8dc3fedffa136ff5acc742979de4
midori-devsacademy <shibukawa.midori@gmail.com>
2021-08-12 08:28:48 -0300

    minor fixes and more CSS clean up

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ const themeStyle = document.getElementById('theme-style');
 const socialMedia = document.getElementsByClassName('social-media');
 
 themeToggle.onclick = function() {
-    console.log('test');
     if (themeStyle.href.endsWith('css/light-theme.css')) {
        themeStyle.href = 'css/dark-theme.css';
        Array.from(socialMedia).forEach(element => {
