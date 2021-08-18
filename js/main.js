6510d427c71e17510d2189a508e0bf2856b32ced
midori-devsacademy <shibukawa.midori@gmail.com>
2021-08-18 10:41:00 -0300

    added gitignore and transition effect

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
-const themeToggle = document.getElementById('change-theme');
-const themeStyle = document.getElementById('theme-style');
-const socialMedia = document.getElementsByClassName('social-media');
+window.onload = () => {
+    const themeToggle = document.getElementById('change-theme');
+    const themeStyle = document.getElementById('theme-style');
+    const socialMedia = document.getElementsByClassName('social-media');
 
-themeToggle.onclick = function() {
-    if (themeStyle.href.endsWith('css/light-theme.css')) {
-       themeStyle.href = 'css/dark-theme.css';
-       Array.from(socialMedia).forEach(element => {
-           element.className = 'social-media dark';
-       });
-    } else {
-        themeStyle.href = 'css/light-theme.css';
+    themeToggle.onclick = function() {
+        if (themeStyle.href.endsWith('css/light-theme.css')) {
+        themeStyle.href = 'css/dark-theme.css';
         Array.from(socialMedia).forEach(element => {
-            element.className = 'social-media light';
+            element.className = 'social-media dark';
         });
-    }
-};
+        } else {
+            themeStyle.href = 'css/light-theme.css';
+            Array.from(socialMedia).forEach(element => {
+                element.className = 'social-media light';
+            });
+        }
+    };
+}
